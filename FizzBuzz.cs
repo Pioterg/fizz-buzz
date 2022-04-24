04fbba9851315e53ca638f7744f3ca687774ffae
Piotr <piotrsitkowski@wp.pl>
2022-04-24 16:12:41 +0200

    Upload Files

---
+++ FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public int number = GetNumber();
+        public string response;
+
+        public static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś złą wartość. Spróbuj ponownie.");
+                    continue;
+                }
+                return number;
+            }
+        }
+        public string DivisibleNumber()
+        {
+                if (number % 5 == 0 && number % 3 == 0)
+                {
+                    response = "FizzBuzz";
+                }
+                else if (number % 3 == 0)
+                {
+                    response = "Fizz";
+                }
+                else if (number % 5 == 0)
+                {
+                    response = "Buzz";
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            return response;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę całkowitą podzielną przez 3 lub 5 a ja Ci coś powiem: ");
+            FizzBuzz selectedNumber = new FizzBuzz();
+            var c  = selectedNumber.DivisibleNumber();
+            Console.WriteLine(c);
+        }
+    }
+}
